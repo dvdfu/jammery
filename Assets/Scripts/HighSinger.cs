fa10b5a83b2ae20ceb75b8ea138b47ac00cff262
dvdfu <davidf1212@gmail.com>
2020-06-24 22:16:22 -0700

    High note colour lerp

---
+++ Assets/Scripts/HighSinger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighSinger : MonoBehaviour {
+    [SerializeField] InstrumentData instrument = null;
+    [SerializeField] SpriteRenderer baseSprite = null;
+    [SerializeField] Color yellow = Color.white;
+    [SerializeField] Color red = Color.white;
+
+    void LateUpdate() {
+        float offset = instrument.highOffset;
+        if (offset > 0) {
+            baseSprite.color = BlendColors(yellow, Color.white, offset / 4);
+        } else if (offset < 0) {
+            baseSprite.color = BlendColors(yellow, red, -offset / 4);
+        } else {
+            baseSprite.color = yellow;
+        }
+    }
+
+    Color BlendColors(Color a, Color b, float amount) {
+        return a + (b - a) * amount;
+    }
+}
